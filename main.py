81571fef1da317ebebac7f3566e5710d68fbe8b9
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-02-17 20:00:49 +0000

    confirm

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         if opcion < 1 or opcion > 24: # Verificar que la opcion elegida sea valida
             print("Opcion incorrecta, seleccione otra opcion")
 
-# Opcion definida para realizar operaciones de suma.
+# Opcion 1 esta definida para realizar operaciones de suma.
         elif opcion == 1:
             print("Has elegido el camino de la suma")
             n1 = int(input("Favor de ingresar el primer numero a sumar: "))
@@ -64,7 +64,7 @@ if __name__ == "__main__":
             print(f"El resultado de la suma es: {suma(n1, n2)}") # Se muestra el resultado de la suma.
             guardar_historial(historial, f"Suma: {n1} + {n2} = {suma(n1, n2)}")
 
-# Esta opcion permite realizar operaciones de resta.
+# Opcion 2 permite realizar operaciones de resta.
         elif opcion == 2:
             print("Has elegido el camino de la resta")
             n1 = int(input("Favor de ingresar el primer numero a restar: "))
@@ -72,21 +72,26 @@ if __name__ == "__main__":
             print(f"El resultado de la resta es: {resta(n1, n2)}") # Se muestra el resultado de la resta.
             guardar_historial(historial, f"Resta: {n1} - {n2} = {resta(n1, n2)}")
 
+# Opcion 3 es la opcion que permite realizar multiplicaciones.
         elif opcion == 3:
             print("Has elegido el camino de la multiplicacion")
             n1 = int(input("Favor de ingresar el primer numero a multiplicar: "))
             n2 = int(input("Favor de ingresar el segundo numero a multiplicar: "))
             print(f"El resultado de la multiplicacion es: {multiplicacion(n1, n2)}") # Se muestra el resultado de la multiplicacion.
             guardar_historial(historial, f"Multiplicacion: {n1} x {n2} = {multiplicacion(n1, n2)}")
-            
+
+# Opcion 4 permite realizar divisiones, utilizando while primero para validar sus numeros y evitar realizar operaciones con 0.            
         elif opcion == 4:
             print("Has elegido el camino de la division")
             while True:
                 try:
                     n1 = float(input("Favor de ingresar el primer numero a dividir: "))
-                    break
+                    if n1 == 0:
+                        print("No se puede dividir el 0")
+                    else:
+                        break
                 except ValueError: # Funciona para que el programa no se rompa y continue, permitiendole al usuario ingresar un numero valido.
-                    print("No se puede dividir entre 0. Intenta con otro numero")
+                    print("Invalido. Intenta con otro numero")
             while True:
                 try:
                     n2 = float(input("Favor de ingresar el segundo numero a dividir: "))
@@ -98,12 +103,13 @@ if __name__ == "__main__":
                     print("Invalido. Intenta con otro numero")
             print(f"El resultado de la division es: {division(n1, n2):.1f}") # Se muestra el resultado de la division con 4 decimales
             guardar_historial(historial, f"Division: {n1} / {n2} = {division(n1, n2):.1f}")
-        
+
+# Opcion 5 su funcion es calcular la raiz cuadrada de un numero siempre y cuando el numero no sea negativo.
         elif opcion == 5:
             print("Has elegido el camino de la raiz cuadrada")
             while True:
                 try:
-                    n1 = float(input("Favor de ingresar el numero para sacar su raiz cuadrada: "))
+                    n1 = float(input("Favor de ingresar el numero para calcular su raiz cuadrada: "))
                     if n1 < 0:
                         print("No se puede calcular la raiz cuadrada en numeros negativos.")
                     else:
@@ -112,7 +118,8 @@ if __name__ == "__main__":
                     print("Invalido, ingresa un numero: ")
             print(f"El resultado de la raiz cuadrada es: {sqrt(n1):.4f}") # Se muestra el resultado de la raiz cuadrada con 4 decimales
             guardar_historial(historial, f"Raiz cuadrada: {n1} = {sqrt(n1):.4f}")
-        
+
+# Opcion 6 permite calcular la potencia de un numero, utilizando la funcion pow que fue importada desde math.
         elif opcion == 6:
             print("Has elegido el camino de la potencia")
             n1 = float(input("Favor de ingresar el numero base: "))
@@ -120,48 +127,56 @@ if __name__ == "__main__":
             print(f"El resultado de la potencia es: {pow(n1, n2)}") # Se muestra el resultado de la potencia sin decimales porque la funcion "pow" devuelve un numero entero si el resultado es un numero entero, y un numero decimal si el resultado es un numero decimal. Por lo tanto, no es necesario mostrar el resultado con decimales.
             guardar_historial(historial, f"Potencia: {n1} ^ {n2} = {pow(n1, n2)}")
 
+# Opcion 7 permite calcular el logaritmo de un numero, utilizando la funcion log importada desde math.
         elif opcion == 7:
             print("Has elegido el camino del logaritmo")
-            n1 = float(input("Favor de ingresar el numero para sacar su logaritmo: "))
+            n1 = float(input("Favor de ingresar el numero para calcular su logaritmo: "))
             print(f"El resultado del logaritmo es: {log(n1):.4f}") # Se muestra el resultado del logaritmo con 4 decimales
             guardar_historial(historial, f"Logaritmo: Log{n1} = {log(n1):.4f}")
 
+# Opcion 8 utiliza la funcion exp importada desde math, dicha funcion permite calcular el exponencial de un numero.
         elif opcion == 8:
             print("Has elegido el camino de la exponencial")
-            n1 = float(input("Favor de ingresar el numero para sacar su exponencial: "))
+            n1 = float(input("Favor de ingresar el numero para calcular su exponencial: "))
             print(f"El resultado de la exponencial es: {exp(n1):.4f}") # Se muestra el resultado de la exponencial con 4 decimales
             guardar_historial(historial, f"Exponencial: {n1} = {exp(n1):.4f}")
 
+# Opcion 9 realiza una conversion de km a millas
         elif opcion == 9:
             print("Has elegido la conversion de km a millas: ")
             n1 = float(input("Favor de ingresar los km: "))
             print(f"El resultado de los km a millas es: {km_millas(n1)}") # Se muestra el resultado de la conversion de km a millas.
             guardar_historial(historial, f"Km a Mph: {n1}km = {km_millas(n1):.4f}mph")
 
+# Opcion 10 su funcion es convertir fahrenheit a celsius
         elif opcion == 10:
             print("Has elegido la conversion de fahrenheit a celsius")
             n1 = float(input("Favor de ingresar el numero de los fahrenheit: "))
             print(f"El resultado de la conversion fahrenheit a celsius es: {fahrenheit_celsius(n1)}°C") # Se muestra el resultado de la conversion de fahrenheit a celsius.
             guardar_historial(historial, f"Fahrenheit a Celsius: {n1}°F = {fahrenheit_celsius(n1)}°C")
 
+# Opcion 11 realiza la conversion de Libras a Kilogramos.
         elif opcion == 11:
             print("Has elegido la conversion de Lbs a Kg")
             n1 = float(input("Favor de ingresar la cantidad de las Lbs: "))
             print(f"El resultado de la conversion de Lbs a Kg es: {lbs_kg(n1):.2f}kg")
             guardar_historial(historial, f"Lbs a Kg: {n1}lbs = {lbs_kg(n1):.2f}kg")
 
+# Opcion 12 convierte metros a kilometros.
         elif opcion == 12:
             print("Has elegido la conversion de Mts a Km")
             n1 = float(input("Favor de ingresar la cantidad de los Mts: "))
             print(f"El resultado de la conversion Mts a Km es: {m_km(n1):.4f}km")
             guardar_historial(historial, f"Mts a Km: {n1}mts = {m_km(n1):.4f}km")
 
+# Opcion 13 realiza conversiones de centimeros a metros.
         elif opcion == 13:
             print("Has elegido la conversion de Cm a Mts")
             n1 = float(input("Favor de ingresar la cantidad de los Cm: "))
             print(f"El resultado de la conversion Cm a Mts es: {cm_m(n1)}mts")
             guardar_historial(historial, f"Cm a Mts: {n1}cm = {cm_m(n1)}mts")
 
+# Opcion 14 su funcion es realizar conversiones de horas a minutos.
         elif opcion == 14:
             print("Has elegido la conversion de Hrs a Min")
             n1 = float(input("Favor de ingresar la cantidad de las Hrs: "))
@@ -174,9 +189,9 @@ if __name__ == "__main__":
             print(f"El resultado de la conversion Ft a Cm es: {pies_cm(n1):.2f}cm")
             guardar_historial(historial, f"Ft a Cm: {n1}ft = {pies_cm(n1):.2f}cm")
 
-# Para las opciones 16, 17 y 18 se solicita al usuario un numero pero primero se convierte ese numero a radianes. Porque las funciones trigonometricas en python trabajan con radianes, por lo que es necesario convertir el numero ingresado a radianes.
+# Para las opciones 16, 17 y 18 se solicitan al usuario un numero pero primero se convierte ese numero a radianes. Porque las funciones trigonometricas en python trabajan con radianes, por lo que es necesario convertir el numero ingresado a radianes.
 
-# Opcion que permite calcular la tangente de un numero, mediante el uso de radiantes, el resultado puede no ser un numero exacto pero si cercano.  
+# Opcion 16 permite calcular la tangente de un numero, mediante el uso de radiantes, el resultado puede no ser un numero exacto pero si cercano.  
         elif opcion == 16:
             print("Has elegido el camino de la tangente")
             n1 = float(input("Favor de ingresar el numero para sacar su tangente: "))
@@ -184,7 +199,7 @@ if __name__ == "__main__":
             print(f"El resultado de la tangente es: {tan(rad):.4f}") # Se muestra el resultado de la tangente con 4 decimales
             guardar_historial(historial, f"Tangente: Tan{n1} = {tan(rad):.4f}")
 
-# Opcion que permite calcular el seno de un numero, mediante el uso de radiantes.
+# Opcion 17 permite calcular el seno de un numero, mediante el uso de radiantes.
         elif opcion == 17:
             print("Has elegido el camino del seno")
             n1 = float(input("Favor de ingresar el numero para sacar su seno: "))
@@ -192,7 +207,7 @@ if __name__ == "__main__":
             print(f"El resultado del seno es: {sin(rad):.4f}") # Se muestra el resultado del seno con 4 decimales
             guardar_historial(historial, f"Seno: Sin{n1} = {sin(rad):.4f}")
 
-# Opcion que permite calcular el coseno de un numero, mediante el uso de radiantes
+# Opcion 18 permite calcular el coseno de un numero, mediante el uso de radiantes
         elif opcion == 18:
             print("Has elegido el camino del coseno")
             n1 = float(input("Favor de ingresar el numero para sacar su coseno: "))
@@ -200,7 +215,7 @@ if __name__ == "__main__":
             print(f"El resultado del coseno es: {cos(rad):.4f}") # Se muestra el resultado del coseno con 4 decimales
             guardar_historial(historial, f"Coseno: cos{n1} = {cos(rad):.4f}")
 
-# Esta opcion permite calcular el area de un rectangulo
+# Opcion 19 creada para calcular el area de un rectangulo
         elif opcion == 19:
             print("Has elegido el camino para calcular el Area de un Rectangulo")
             n1 = float(input("Favor de ingresar la base del rectangulo:"))
@@ -208,14 +223,14 @@ if __name__ == "__main__":
             print(f"El area del rectangulo es: {multiplicacion(n1, n2)}")
             guardar_historial(historial, f"Area de un Rectangulo: {n1}b * {n2}h = {multiplicacion(n1, n2):.2f}")
 
-# Esta opcion permite calcular el área de un circulo.
+# Opcion 20 permite calcular el área de un circulo mediante la constante pi importada desde math.
         elif opcion == 20:
             print("Has elegido el camino para calcular el Area de un Circulo")
             n1 = float(input("Favor de ingresa el radio del circulo:"))
             print(f"El area del circulo es: {pi * (n1**2):.4f}")
-            guardar_historial(historial, f"Area de un Circulo: {n1} x {pi} = {pi * (n1**2):.4f}")
+            guardar_historial(historial, f"Area de un Circulo: {n1}² x {pi} = {pi * (n1**2):.4f}")
 
-# Esta opcion permite realizar dos operaciones al mismo tiempo, primero se haria la suma de dos numeros y posteriormente se sacaria la raiz cuadrada de esos dos numeros.
+# Opcion 21 permite realizar dos operaciones al mismo tiempo, primero realiza la suma de dos numeros y posteriormente calcula la raiz cuadrada del resultado de la suma.
         elif opcion == 21:
             print ("Has elegido el modo combo suma + raiz cuadrada")
             n1 = int(input("Favor de ingresar el primer numero a sumar: "))
@@ -224,7 +239,7 @@ if __name__ == "__main__":
             print(f"El resultado de la suma es: {suma(n1, n2)} y la raiz cuadrada del resultado es: {sqrt(resultado_suma)}") 
             guardar_historial(historial, f"Suma: {n1} + {n2} = {suma(n1, n2)} y Raiz cuadrada: √{suma(n1, n2)} = {sqrt(resultado_suma)}")
 
-# Esta es un opcion que determina el area y el precio total de un terreno
+# Opcion 22 determina el area y el precio total de un terreno, realiza dos multiplicaciones.
         elif opcion == 22:
             print("Has elegido el Modo Mision (Terreno)")
             while True:
@@ -258,7 +273,7 @@ if __name__ == "__main__":
             print(f"Área del terreno: {multiplicacion(n1,n2):.4f}m² Precio total: ${multiplicacion(area, n3):.2f} ")
             guardar_historial(historial, f"Modo Mision (Terreno): Area = {n1} x {n2} = {multiplicacion(n1, n2)} Precio total = ${multiplicacion(area, n3):.2f}")
 
-# Opcion creada para obtener el historial de las operaciones y funciones utilizadas
+# Opcion 23 creada para obtener el historial de las operaciones y funciones utilizadas.
         elif opcion == 23:
             print("--- Historial ---")
             if historial:
