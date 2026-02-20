3f91d4ad1abf00303d48a68d2063c27e5263047d
SusanaJanine <susanajaninevelezflores@gmail.com>
2026-02-20 16:50:15 +0000

    confirm

--- ProyectoFinal/IMC_Calculos.py
+++ ProyectoFinal/IMC_Calculos.py
@@ -1,5 +1,10 @@
 from math import pow
 
-def calcular_imc(peso_kg, altura_m):
-    imc = peso_kg / pow(altura_m, 2)
+def validacion_datos(peso_kg, altura_cm):
+    if peso_kg <= 0 or altura_cm <= 0:
+        return False
+    return True
+
+def calcular_imc(peso_kg, altura_cm):
+    imc = peso_kg / pow(altura_cm, 2)
     return imc
